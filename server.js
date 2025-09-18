4902060be197037faa3dfddb53be78dd627f5fb3
tanmaysoin5-netizen <tanmaysoin5@gmail.com>
2025-09-18 15:19:18 +0530

    practical 1 changes

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 var http = require('http');
-
 var fs = require('fs');
 
-http.createServer (function (req, res) {
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-res writeHead(200, {'Content-Type': 'text/plain'});
-// write a file
+  // Write a file
+  fs.writeFile("demo.txt", "Hello, I am learning Node.js!", function (err) {
+    if (err) {
+      res.end("Error writing file");
+    } else {
+      // Read the file
+      fs.readFile("demo.txt", "utf8", function (err, data) {
+        if (err) {
+          res.end("Error reading file");
+        } else {
+          res.end("File Content: " + data);
+        }
+      });
+    }
+  });
 
-fs writeFile("demo.txt", "Hello, I am learning Node-js!"
-, function (err){
+}).listen(4000);
 
-if (err)
-
-res. end("Error writing file");
-else
-read the file
-ts- readFile("demo.txt", "utf8", function(err, data)f
-if
-(err) {
-res. end ("Error reading file");
-else
-res. end ("File Content: " + data);
-}). listen (4000);
-console. log ("Server is running at http://localhost:4000");
+console.log("Server is running at http://localhost:4000");
